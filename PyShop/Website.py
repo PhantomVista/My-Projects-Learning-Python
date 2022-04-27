1d0102ca86a16d6e31aaf1f2f91dd484dc2402a7
PhantonVista <sirtay09@gmail.com>
2022-04-27 06:55:21 -0400

    Using Django to Create a Website. 4/27 -Python -SQL Lite -Django -Pycharm -Github

---
+++ PyShop/Website.py
@@ -0,0 +1,4 @@
+# Creating a Website using Django
+# type in terminal "django-admin startproject pyshop" to create pyshop app
+# Type "python manage.py runserver" in terminal to start website
+# Type "python manage.py startapp products" to add products app
