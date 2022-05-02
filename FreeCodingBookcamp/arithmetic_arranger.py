8decce4d28792b72ac68d2ca3c08b3a076dad357
PhantonVista <sirtay09@gmail.com>
2022-05-02 07:14:18 -0400

    Training "FreeCodeCamp"

--- FreeCodingBookcamp/arithmetic_arranger.py
+++ FreeCodingBookcamp/arithmetic_arranger.py
@@ -1,3 +1,7 @@
 arithmetic_arranger = ([32 + 698, 3801 - 2, 45 + 43, 123 + 49, 22 // 3, 4 + 4])
 
 print(arithmetic_arranger)
+
+arithmetic_arranger_1 = (["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"], True)
+
+print(arithmetic_arranger_1)
