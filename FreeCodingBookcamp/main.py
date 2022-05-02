e879aa54e893d0991134aa161a01c4c6ad33b421
PhantonVista <sirtay09@gmail.com>
2022-05-02 07:36:16 -0400

    Training "FreeCodeCamp"

--- FreeCodingBookcamp/main.py
+++ FreeCodingBookcamp/main.py
@@ -1,6 +1,41 @@
-#
+# Conditional Structures
 
-def say_hi():
-    print("Hello User")
+x = 7
+if x < 2:
+    print("small")
+elif x < 10:
+    print("Medium")
+else:
+    print("LARGE")
+print('All Done!')
 
-say_hi()
+# Try and Except
+
+astr = 'Hello Bob'
+try:
+    istr = int(astr)
+except:
+    istr = 1
+
+print('First, istr')
+
+astr = '123'
+try:
+    istr = int(astr)
+except:
+    istr = -1
+
+print('Second', astr)
+
+# Users input for Try/Except
+
+rawstr = input("Enter a number: ")
+try:
+    ival = int(rawstr)
+except:
+    ival = -1
+
+if ival > 0:
+    print("Nice Work")
+else:
+    print('Not a number')
