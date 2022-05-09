492356bed9c311545f3c55c75d6678d01e5acfb0
PhantonVista <sirtay09@gmail.com>
2022-05-09 07:15:55 -0400

    Linkedin Learning Python 5.9.2022

--- pythonProject/Dictionaries.py
+++ pythonProject/Dictionaries.py
@@ -3,7 +3,7 @@ cats = {"Jane": 6, "Tom": 14,"Sara": 8}
 print(cats["Jane"])
 
 
-#Challenge: Create a dictionary with Ints for keys and Booleans for Values
+# Challenge: Create a dictionary with Ints for keys and Booleans for Values
 
 cool = {1: True, 2: False, 4: True}
 
